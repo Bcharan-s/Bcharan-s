9563105e6d3dbd6003caa11fb6a72794a7942c40
charan <charan.charansmart55@gmail.com>
2024-11-05 23:11:14 +0530

    changes to docker_file

--- docker/main.go
+++ docker/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+type PageVariables struct {
+	Title string
+	Body  string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hi there!, Welcome to Go!")
+	pageVariables := PageVariables{
+		Title: "Welcome to Go Web Development",
+		Body:  "This is a simple Go web application that uses HTML templates.",
+	}
+
+	tmpl, err := template.New("index").Parse(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		<p>{{.Body}}</p>
+	</body>
+	</html>
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tmpl.Execute(w, pageVariables)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Failed to start server:", err)
-	}
+
+	log.Println("Starting web server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
