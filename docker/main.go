7f94de5ecde50ae11b8e49834f667216bfae7b16
charan <charan.charansmart55@gmail.com>
2024-11-07 11:06:29 +0530

    muti stage docker file
    :wq!

--- docker/main.go
+++ docker/main.go
@@ -1,47 +1,15 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-type PageVariables struct {
-	Title string
-	Body  string
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	pageVariables := PageVariables{
-		Title: "Welcome to Go Web Development",
-		Body:  "This is a simple Go web application that uses HTML templates.",
-	}
-
-	tmpl, err := template.New("index").Parse(`
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>{{.Title}}</title>
-	</head>
-	<body>
-		<h1>{{.Title}}</h1>
-		<p>{{.Body}}</p>
-	</body>
-	</html>
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = tmpl.Execute(w, pageVariables)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Fprintf(w, "Hello, World!")
 }
-
 func main() {
 	http.HandleFunc("/", handler)
-
-	log.Println("Starting web server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
 }
